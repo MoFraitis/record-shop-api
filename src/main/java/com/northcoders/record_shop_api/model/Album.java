88339816c7f158baf31b2cc32c720ab525442e80
Mohamed Fraitis <mfraitis1@gmail.com>
2024-12-06 11:59:16 +0000

    refactor: refactored Album model (lombok was not producing getters and setters)

--- src/main/java/com/northcoders/record_shop_api/model/Album.java
+++ src/main/java/com/northcoders/record_shop_api/model/Album.java
@@ -2,29 +2,30 @@ package com.northcoders.record_shop_api.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
 
-import java.time.LocalDate;
+//import java.time.LocalDate;
 
 
+//@Getter
 @Entity
-@Table(name = "album")
-@Getter
-@Setter
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Table
+//@Getter
+//@Setter
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Builder
 public class Album {
 
-    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue
     @Column(updatable = false, nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
 
     @Column
-    String name;
+    String albumName;
 
     @Column
     String artistName;
@@ -35,8 +36,69 @@ public class Album {
     Long releaseYear;
 
     @Column
+    @Enumerated(EnumType.ORDINAL)
     Genre genre;
 
     @Column
     Integer quantityInStock;
+
+    public Album(Long id, String albumName, String artistName, Long releaseYear, Genre genre, Integer quantityInStock) {
+        this.id = id;
+        this.albumName = albumName;
+        this.artistName = artistName;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+        this.quantityInStock = quantityInStock;
+    }
+
+    public Album() {
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(Integer quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public Long getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Long releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
