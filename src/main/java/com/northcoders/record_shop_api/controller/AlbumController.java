1512cce11a58a32ad2fbcde34cd1a22c98aa63df
Mohamed Fraitis <mfraitis1@gmail.com>
2024-12-06 09:26:33 +0000

    feat: add AlbumController with GET methods

---
+++ src/main/java/com/northcoders/record_shop_api/controller/AlbumController.java
@@ -0,0 +1,31 @@
+package com.northcoders.record_shop_api.controller;
+
+import com.northcoders.record_shop_api.model.Album;
+import com.northcoders.record_shop_api.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/album")
+public class AlbumController {
+
+    @Autowired
+    AlbumService albumService;
+
+    @GetMapping
+    public ResponseEntity<List<Album>> getAllAlbums() {
+        return new ResponseEntity<>(albumService.getAllAlbums(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(albumService.getAlbumById(id), HttpStatus.OK);
+    }
+}
